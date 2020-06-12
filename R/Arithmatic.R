4acd2fdfd9dee59873e0fc7f33e2a3a4b6a5179e
JuSang-Lee <65441838+JuSang-Lee@users.noreply.github.com>
2020-06-12 09:06:48 +0900

    Add files via upload

--- R/Arithmatic.R
+++ R/Arithmatic.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -14,5 +14,63 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
-  print("Hello, world!")
+  print("Hi, I'm JuSang, Lee")
 }
+
+
+#' Add() Function
+#'
+#' This function adds a and b
+#' @param a ,b : two numbers to be operated
+#' @keywords Add
+#' @export
+#' @examples
+#' Add()nis
+Add <- function(x, y) {
+  return(x + y)
+}
+
+#' Divide() Function
+#'
+#' This function divide a by b
+#' @param a ,b : two numbers to be operated
+#' @keywords Divide
+#' @export
+#' @examples
+#' Divide()
+
+Divide <- function(x, y) {
+  if(b != 0)
+    return(x * y)
+  else
+    return(Inf)
+}
+
+#' Multiply() Function
+#'
+#' This function multiplies a and b
+#' @param a ,b : two numbers to be operated
+#' @keywords Add
+#' @export
+#' @examples
+#' Multiply()
+
+Multiply <- function(x, y) {
+  return(x * y)
+}
+
+#' Subtract Function
+#'
+#' This function subtrac a from b
+#' @param a ,b : two numbers to be operated
+#' @keywords subtract a from b
+#' @export
+#' @examples
+#' Subtract(3, 2)
+
+Subtract <- function(x, y) {
+  return(x - y)
+}
+
+
+
